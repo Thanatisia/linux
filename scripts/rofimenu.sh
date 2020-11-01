7902bb98b8279c02cac00fb8085b63e5020bbb90
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2020-11-01 18:49:50 +0800

    "2020-11-01 18:49H, Modified 'rofimenu.sh'"

--- scripts/rofimenu.sh
+++ scripts/rofimenu.sh
@@ -5,4 +5,19 @@
 # Import rofiutil.sh library
 . rofiutils.sh
 
-echo "$(rofi_dmenu "HelloWorld\nHello World")"
+# -- Global variables
+options_array=("Hello World" "HelloWorld" "Hello")
+options_str="HelloWorld\nHello World\nHello"
+
+# -- Functions
+function join_str()
+{
+	for opt in "${options_array[@]}"; do
+		options+="$opt\n"
+	done
+	echo "$options"
+}
+
+options="$(join_str)"
+selected_option="$(rofi_dmenu "$options")"
+echo "Selected: $selected_option"
